544948dbca39dd1ad125d638cd70af92b838f0cf
hustle-coin <94656438+hustle-coin@users.noreply.github.com>
2022-01-25 23:45:33 -0500

    Update expedition.service.ts

--- server/src/expedition/expedition.service.ts
+++ server/src/expedition/expedition.service.ts
@@ -20,6 +20,7 @@ type ExpeditionQueryResult = {
   id: number;
   name: string;
   beginDateTime: Date;
+  hasMutatedOrb: hasMutatedOrb;
   User: User;
   ExpeditionParticipant: (ExpeditionParticipant & { User: User })[];
 };
